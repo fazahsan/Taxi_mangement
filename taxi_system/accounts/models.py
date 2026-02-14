ea4f653ace38127325e8d7f58c2394e1809ab60b
fazahsan <ahsan_732000@yahoo.com>
2026-02-14 16:43:13 +0100

    models

--- taxi_system/accounts/models.py
+++ taxi_system/accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Driver(models.Model):
+    usario=models.OneToOneField(User,on_delete=models.CASCADE)
+    Telefono=models.CharField(max_length=20)
+    No_Licencia=models.CharField(max_length=20)
+    Matricula=models.CharField(max_length=20)
+    Activado=models.BooleanField(default=True)
+    def __str__(self):
+        return self.usario.username
--- taxi_system/trips/models.py
+++ taxi_system/trips/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import Driver
 
 # Create your models here.
+class Trip(models.Model):
+    STATUS_CHOICES=[('encourso','Encourso'),('completado','Completado')]
+    conductor=models.ForeignKey(Driver,on_delete=models.CASCADE)
+    lugar_de_recogida=models.CharField(max_length=50)
+    destino=models.CharField(max_length=50)
+    hora_inicio=models.DateTimeField(auto_now_add=True)
+    hora_final=models.DateTimeField(null=True,blank=True)
+    distancia=models.FloatField(null=True,blank=True)
+    dinero=models.DecimalField(max_digits=8,decimal_places=2,null=True,blank=True)
+    estatuto=models.CharField(max_length=20,choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.conductor} - {self.estatuto}"
